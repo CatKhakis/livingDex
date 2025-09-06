34851924ee174fd2aa2338bab24c6cad675ae385
CatKhakis <violet@funkycat.media>
2025-09-06 11:30:28 -0500

    gridlist

--- src/index.js
+++ src/index.js
@@ -1,25 +1,84 @@
-import _ from 'lodash';
 import printMe from './print.js';
 
 import './style.css';
 
+
 const Pokedex = require("pokeapi-js-wrapper")
-const P = new Pokedex.Pokedex()
+const customOptions = {
+  cache: true,
+  timeout: 5 * 1000, // 5s
+  cacheImages: true
+}
+const pokedex = new Pokedex.Pokedex(customOptions)
+
+
+const gridContainer = document.createElement('div');
+gridContainer.id = 'gridContainer';
+
+
 
 function component() {
   const element = document.createElement('div');
-  const btn = document.createElement('button');
 
   // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack !'], ' ');
+  element.innerHTML = 'pogeymon';
   element.classList.add('hello');
-
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
 
   return element;
 }
 
 document.body.appendChild(component());
+document.body.appendChild(gridContainer);
+
+
+pokedex.getPokedexsList()
+    .then(function(response) {
+        console.log(response);
+    }
+)
+
+pokedex.getPokedexByName("updated-unova")
+    .then(async function(response) {
+
+        for (const entry of response.pokemon_entries) {
+
+            let name = entry.pokemon_species.name;
+            let url = entry.pokemon_species.url;
+
+            const pokemon = document.createElement('div');
+            pokemon.classList.add('pokemonTile');
+
+            const topInfo = document.createElement('div');
+            topInfo.classList.add('topInfo');
+            
+            const idText = document.createElement('div');
+            idText.classList.add('idText');
+
+            const indexHolder = document.createElement('div');
+            indexHolder.classList.add('indexHolder');
+
+            let id = url.substring(0, url.length - 1)
+            id = id.substring(id.lastIndexOf('/') + 1);
+
+            idText.innerHTML = id;
+
+            const img = document.createElement('img');
+            img.classList.add('gridSprite');
+
+            topInfo.appendChild(idText);
+            topInfo.appendChild(indexHolder);
+
+            pokemon.appendChild(topInfo);
+            
+            pokemon.appendChild(img);
+            gridContainer.appendChild(pokemon);
+
+            pokedex.getPokemonByName(id)
+                .then(function(response) {
+
+                    img.src = response.sprites.front_default;
+                }
+            )
+        }
+    }
+)
